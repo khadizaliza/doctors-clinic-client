804e8090d207b2d6fe01a0ec2e71e9f6519ce309
khadizaliza <khadizaakter19836@gmail.com>
2022-05-16 02:37:46 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#198F85",
+          secondary: "#D926A9",
+          accent: "#1A242B",
+          neutral: "#FBBD23",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
